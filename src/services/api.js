2a12a4d2375b9ad8ae02af1473926cda9eb6e54d
Andre Peixoto <peixoto.amp@gmail.com>
2020-12-10 23:59:32 -0300

    back-end api implemented

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://localhost:3330',
+});
+
+export default api;
+
+/**
+ * iOS with emulator: localhost
+ * iOS with device: your machine's IP
+ * Android with Emulator: localhost (adb reverse)
+ * Android with Emulator: 10.0.2.2 (Android Studio)
+ * Android with device: your machine's IP
+ */
