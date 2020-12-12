7e4d4962df9bcf171762d3a1984896eccd532cdc
Andre Peixoto <peixoto.amp@gmail.com>
2020-12-11 23:36:50 -0300

    add project function/button implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from './services/api';
 
 // View: div, footer, header, etc (containers)
 // Text: p, span, strong, h1, h2...
@@ -7,11 +17,54 @@ import {View, Text, StyleSheet, StatusBar} from 'react-native';
 // All components have display: flex
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `New Project ${Date.now()}`,
+      owner: 'Andre Peixoto',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="purple" />
+
+      {/* <View style={styles.container}>
+        <Text style={styles.title}>Projects' List</Text>
+        {projects.map((project) => (
+          <Text key={project.id} style={styles.project}>
+            {project.title}
+          </Text>
+        ))}
+      </View> */}
+
       <View style={styles.container}>
-        <Text style={styles.title}>Hi!</Text>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Add Project</Text>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -20,13 +73,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#9e9e9e',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   title: {
     color: '#fff',
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  project: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
